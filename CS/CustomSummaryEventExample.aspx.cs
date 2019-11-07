a8ea612b3a1c3d3e49626da9c55b7ede5a0ef398
BrianDX <boris.zaitsev@devexpress.com>
2019-11-07 19:06:59 +0300

    add comments to the code

--- CS/CustomSummaryEventExample.aspx.cs
+++ CS/CustomSummaryEventExample.aspx.cs
@@ -17,22 +17,39 @@ public partial class _Default : System.Web.UI.Page
     protected void ASPxPivotGrid1_CustomSummary(object sender, DevExpress.Web.ASPxPivotGrid.PivotGridCustomSummaryEventArgs e)
     {
         ASPxPivotGrid pivot = (ASPxPivotGrid)sender;
+        // In the root IF clause, determine the summary type.
+        // All fields at the level lower than the "Company" should show Min values.
+        // All fields are located in the Row Area, so it is necessary to check the AreaIndex property of the e.RowField.
         if (e.RowField != null && e.RowField.AreaIndex >= fieldCompanyName.AreaIndex)
-        {            
+        {
+            // The Pivot Grid automatically calculates predefined summary values for all cells.
+            // To obtain summary values, use the e.SummaryValue to get the SummaryValue object,
+            // get the Min value and assign it to the CustomValue property.
             e.CustomValue = e.SummaryValue.Min;
         }
         else
         {
+            // The Pivot Grid calculates summary values for all cells based on the underlying data source rows regardless of the cell type.
+            // However, the current scenario calculates total values based on the low level cell summary values.
+            // For the Year field rows it is necessary to group data by Company, 
+            // find Min value in each group and finally summarize all Min values.
+            // For the Grand Total cell it is necessary to group data by the Year, Quarter and Company values. 
+            // 
+            // To get grouping fields, iterate through the Fields collection 
+            // and find the fields located in the Row Area below the current e.RowField but above the Company field.
             var groupingFields = pivot.Fields.Cast<PivotGridField>().Where(f => f.Visible && f.Area == fieldCompanyName.Area && f.AreaIndex <= fieldCompanyName.AreaIndex && (e.RowField == null || f.AreaIndex > e.RowField.AreaIndex));
+            // Get the underlying data.
             var drillDownDataSource = e.CreateDrillDownDataSource().Cast<PivotDrillDownDataRow>();
             List<IEnumerable<PivotDrillDownDataRow>> temporaryList = new List<IEnumerable<PivotDrillDownDataRow>>();
             temporaryList.Add( drillDownDataSource  );
             IEnumerable<IEnumerable<PivotDrillDownDataRow>> dataGroups = temporaryList;
+            // Group the entire data source by the recently obtained groupingFields:
             foreach (var field in groupingFields)
             {
                 var localField = field;
                 dataGroups = dataGroups.SelectMany(groupRows => groupRows.GroupBy(r => r[localField]).Select(g => g.AsEnumerable()));
             }
+            // The final step is to iterate through all groups, find a Min value in each group and calculate the sum of all Min values.
             var value = dataGroups.Sum(groupRows => groupRows.Min( r => Convert.ToDecimal(r[e.FieldName])));
             e.CustomValue = value;
         }
--- VB/CustomSummaryEventExample.aspx.vb
+++ VB/CustomSummaryEventExample.aspx.vb
@@ -17,20 +17,37 @@ Partial Public Class _Default
 
 
 	Protected Sub ASPxPivotGrid1_CustomSummary(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxPivotGrid.PivotGridCustomSummaryEventArgs)
-		Dim pivot As ASPxPivotGrid = DirectCast(sender, ASPxPivotGrid)
-		If e.RowField IsNot Nothing AndAlso e.RowField.AreaIndex >= fieldCompanyName.AreaIndex Then
-			e.CustomValue = e.SummaryValue.Min
-		Else
-			Dim groupingFields = pivot.Fields.Cast(Of PivotGridField)().Where(Function(f) f.Visible AndAlso f.Area = fieldCompanyName.Area AndAlso f.AreaIndex <= fieldCompanyName.AreaIndex AndAlso (e.RowField Is Nothing OrElse f.AreaIndex > e.RowField.AreaIndex))
-			Dim drillDownDataSource = e.CreateDrillDownDataSource().Cast(Of PivotDrillDownDataRow)()
+        Dim pivot As ASPxPivotGrid = DirectCast(sender, ASPxPivotGrid)
+        ' In the root IF clause, determine the summary type.
+        ' All fields at the level lower than the "Company" should show Min values.
+        ' All fields are located in the Row Area, so it Is necessary to check the AreaIndex property of the e.RowField.
+        If e.RowField IsNot Nothing AndAlso e.RowField.AreaIndex >= fieldCompanyName.AreaIndex Then
+            ' The Pivot Grid automatically calculates predefined summary values for all cells.
+            ' To obtain summary values, use the e.SummaryValue to get the SummaryValue object,
+            ' get the Min value And assign it to the CustomValue property.
+            e.CustomValue = e.SummaryValue.Min
+        Else
+            ' The Pivot Grid calculates summary values for all cells based on the underlying data source rows regardless of the cell type.
+            ' However, the current scenario calculates total values based on the low level cell summary values.
+            ' For the Year field rows it Is necessary to group data by Company, 
+            ' find Min value in each group And finally summarize all Min values.
+            ' For the Grand Total cell it Is necessary to group data by the Year, Quarter And Company values. 
+            ' 
+            ' To get grouping fields, iterate through the Fields collection 
+            ' And find the fields located in the Row Area below the current e.RowField but above the Company field.
+            Dim groupingFields = pivot.Fields.Cast(Of PivotGridField)().Where(Function(f) f.Visible AndAlso f.Area = fieldCompanyName.Area AndAlso f.AreaIndex <= fieldCompanyName.AreaIndex AndAlso (e.RowField Is Nothing OrElse f.AreaIndex > e.RowField.AreaIndex))
+            ' Get the underlying data.
+            Dim drillDownDataSource = e.CreateDrillDownDataSource().Cast(Of PivotDrillDownDataRow)()
 			Dim temporaryList As New List(Of IEnumerable(Of PivotDrillDownDataRow))()
 			temporaryList.Add(drillDownDataSource)
-			Dim dataGroups As IEnumerable(Of IEnumerable(Of PivotDrillDownDataRow)) = temporaryList
-			For Each field In groupingFields
-				Dim localField = field
-				dataGroups = dataGroups.SelectMany(Function(groupRows) groupRows.GroupBy(Function(r) r(localField)).Select(Function(g) g.AsEnumerable()))
-			Next field
-			Dim value = dataGroups.Sum(Function(groupRows) groupRows.Min(Function(r) Convert.ToDecimal(r(e.FieldName))))
+            Dim dataGroups As IEnumerable(Of IEnumerable(Of PivotDrillDownDataRow)) = temporaryList
+            ' Group the entire data source by the recently obtained groupingFields:
+            For Each field In groupingFields
+                Dim localField = field
+                dataGroups = dataGroups.SelectMany(Function(groupRows) groupRows.GroupBy(Function(r) r(localField)).Select(Function(g) g.AsEnumerable()))
+            Next field
+            ' The final step is to iterate through all groups, find a Min value in each group and calculate the sum of all Min values.
+            Dim value = dataGroups.Sum(Function(groupRows) groupRows.Min(Function(r) Convert.ToDecimal(r(e.FieldName))))
 			e.CustomValue = value
 		End If
 	End Sub
